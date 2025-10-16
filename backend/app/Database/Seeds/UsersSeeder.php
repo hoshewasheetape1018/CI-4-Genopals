aace3724f3e95a8d450c3ab816f80d9502143c92
hosheewashitape <167269660+hoshewasheetape1018@users.noreply.github.com>
2025-10-17 04:32:44 +0800

    fix(user table seeder): Added a seeder for new users table

--- backend/app/Database/Seeds/UsersSeeder.php
+++ backend/app/Database/Seeds/UsersSeeder.php
@@ -10,33 +10,39 @@ class UsersSeeder extends Seeder
     {
         $now = date('Y-m-d H:i:s');
 
-        $users = [
+        // if you want password that is hashed
+        $adminPassword = password_hash('admin123', PASSWORD_DEFAULT);
+        $userPassword = password_hash('user123', PASSWORD_DEFAULT);
+
+        $userData = [
             [
-                'username'      => 'Nova',
-                'display_name'  => 'Nova Celeste',
-                'level'         => 5,
-                'coins'         => 200,
-                'created_at'    => $now,
-                'updated_at'    => $now,
+                'username'        => 'admin',
+                'display_name'    => 'Administrator',
+                'email'           => 'admin@genopals.com',
+                'password_hash'   => $adminPassword,
+                'coins'           => 9999,
+                'type'            => 'admin',
+                'account_status'  => 1,
+                'email_activated' => 1,
+                'profile_image'   => 'uploads/default_admin.png',
+                'created_at'      => $now,
+                'updated_at'      => $now,
             ],
             [
-                'username'      => 'Kiyo',
-                'display_name'  => 'Kiyo Sato',
-                'level'         => 2,
-                'coins'         => 50,
-                'created_at'    => $now,
-                'updated_at'    => $now,
-            ],
-            [
-                'username'      => 'Umcha',
-                'display_name'  => 'Umcha',
-                'level'         => 10,
-                'coins'         => 9999,
-                'created_at'    => $now,
-                'updated_at'    => $now,
-            ],
+                'username'        => 'testuser',
+                'display_name'    => 'Test User',
+                'email'           => 'user@genopals.com',
+                'password_hash'   => $userPassword,
+                'coins'           => 100,
+                'type'            => 'client',
+                'account_status'  => 1,
+                'email_activated' => 0,
+                'profile_image'   => 'uploads/default_user.png',
+                'created_at'      => $now,
+                'updated_at'      => $now,
+            ]
         ];
 
-        $this->db->table('users')->insertBatch($users);
+        $this->db->table('users')->insertBatch($userData);
     }
 }
