0aa8006e72fae7a638b69004ba69c607481f7924
hosheewashitape <167269660+hoshewasheetape1018@users.noreply.github.com>
2025-10-17 05:46:32 +0800

    feat: Add Admin Controller and dashboard view

---
+++ backend/app/Views/admin/dashboard.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<?= view('components/head') ?>
+
+<body>
+
+    <!-- header -->
+    <?= view('components/header') ?>
+
+    <!-- main content -->
+    <section id="signup">
+        <div class="container">
+            <h2>Woah dashboard</h2>
+    
+        </div>
+    </section>
+    </section>
+
+</body>
+
+<!-- footer -->
+<?= view('components/footer') ?>
+
+</html>
