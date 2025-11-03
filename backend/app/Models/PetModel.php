96e734a8812e26d546f469bd3eb61eb37def39b2
hosheewashitape <167269660+hoshewasheetape1018@users.noreply.github.com>
2025-11-03 17:30:55 +0800

    feat: Added a Entity and Model

---
+++ backend/app/Models/PetModel.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class PetModel extends Model
+{
+    protected $table            = 'pets';
+    protected $primaryKey       = 'id';
+    protected $useAutoIncrement = true;
+    protected $returnType       = '\App\Entities\Pet';
+    protected $useSoftDeletes   = true;
+    protected $protectFields    = true;
+
+    protected $allowedFields    = [
+        'user_id',
+        'name',
+        'species',
+        'image',
+        'base_affection',
+        'base_energy',
+        'base_maintenance',
+        'health',
+        'energy',
+        'hunger'
+    ];
+
+    protected bool $allowEmptyInserts = false;
+    protected bool $updateOnlyChanged = true;
+
+    protected array $casts = [];
+    protected array $castHandlers = [];
+
+    // Dates
+    protected $useTimestamps = true;
+    protected $dateFormat    = 'datetime';
+    protected $createdField  = 'created_at';
+    protected $updatedField  = 'updated_at';
+    protected $deletedField  = 'deleted_at';
+
+    // Validation
+    protected $validationRules      = [];
+    protected $validationMessages   = [];
+    protected $skipValidation       = false;
+    protected $cleanValidationRules = true;
+
+    // Callbacks
+    protected $allowCallbacks = true;
+    protected $beforeInsert   = [];
+    protected $afterInsert    = [];
+    protected $beforeUpdate   = [];
+    protected $afterUpdate    = [];
+    protected $beforeFind     = [];
+    protected $afterFind      = [];
+    protected $beforeDelete   = [];
+    protected $afterDelete    = [];
+}
